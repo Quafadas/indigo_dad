a8c6f05d9b1eaf6686107cbf64323597f11a21d7
MagentaJohn <mrjohnparten@gmail.com>
2024-07-17 18:26:24 +0100

    formatting

--- SceneSplash.scala
+++ SceneSplash.scala
@@ -7,7 +7,6 @@ import scala.collection.View.Empty
 import indigo.shared.materials.Material.ImageEffects
 import indigo.shared.events.PointerEvent.PointerDown
 
-
 object SceneSplash extends Scene[FlicFlacStartupData, FlicFlacGameModel, FlicFlacViewModel]:
 
   var kount2 = 300
@@ -16,7 +15,7 @@ object SceneSplash extends Scene[FlicFlacStartupData, FlicFlacGameModel, FlicFla
   type SceneViewModel = SplashSceneViewModel
 
   val name: SceneName = SceneName("Splash")
-  
+
   def modelLens: Lens[FlicFlacGameModel, FlicFlacGameModel] =
     Lens.keepLatest
 
@@ -33,12 +32,12 @@ object SceneSplash extends Scene[FlicFlacStartupData, FlicFlacGameModel, FlicFla
   def updateModel(
       context: SceneContext[FlicFlacStartupData],
       model: FlicFlacGameModel
-  ): GlobalEvent => Outcome[FlicFlacGameModel] = {
-    case _ => 
-      if (kount2 > 0)
-        println("@@@ SceneSplash-updateModel")
-        kount2 = kount2 - 1    
-      Outcome(model)
+  ): GlobalEvent => Outcome[FlicFlacGameModel] = { case _ =>
+    if kount2 > 0 then
+      println("@@@ SceneSplash-updateModel")
+      kount2 = kount2 - 1
+    end if
+    Outcome(model)
   }
   end updateModel
 
@@ -46,12 +45,13 @@ object SceneSplash extends Scene[FlicFlacStartupData, FlicFlacGameModel, FlicFla
       context: SceneContext[FlicFlacStartupData],
       model: FlicFlacGameModel,
       viewModel: SceneViewModel
-  ): GlobalEvent => Outcome[SceneViewModel] = 
-    case FrameTick =>  
+  ): GlobalEvent => Outcome[SceneViewModel] =
+    case FrameTick =>
       viewModel.update(context.mouse, context.frameContext.inputState.pointers)
 
-    case _ => 
+    case _ =>
       Outcome(viewModel)
+  end updateViewModel
 
   // Show some text
   // When the user clicks anywhere in the screen, trigger an event to jump to the other scene.    val x = context.
@@ -76,64 +76,64 @@ object SceneSplash extends Scene[FlicFlacStartupData, FlicFlacGameModel, FlicFla
       val layerBg = (GameAssets.splashBg)
       val dWidth: Double = width
       val dHeight: Double = height
-      val dsfx : Double = dWidth/1920
-      val dsfy : Double = dHeight/1080
-      //val sf : Double = if ( width * 1080 > height *1920 ) then dsfy else dsfx
-      val sf = 1.0  //FIXME no scaling for the moment and perhaps this is a permanent decision !!!    
+      val dsfx: Double = dWidth / 1920
+      val dsfy: Double = dHeight / 1080
+      // val sf : Double = if ( width * 1080 > height *1920 ) then dsfy else dsfx
+      val sf = 1.0 // FIXME no scaling for the moment and perhaps this is a permanent decision !!!
 
       SceneUpdateFragment(Shape.Box(Rectangle(0, 0, width, height), Fill.Color(RGBA.Black)))
         |+| SceneUpdateFragment(Layer(layerBg.scaleBy(sf, sf)))
-        |+| SceneUpdateFragment(GameAssets.cornerLayers(1920,1080,sf,RGBA.Yellow))  // Splash Scene is 1920x1080
+        |+| SceneUpdateFragment(GameAssets.cornerLayers(1920, 1080, sf, RGBA.Yellow)) // Splash Scene is 1920x1080
         |+| SceneUpdateFragment(textSplash)
 //        |+| SceneUpdateFragment(viewModel.splashButton.draw)
         |+| SceneUpdateFragment(viewModel.rulesButton.draw)
         |+| SceneUpdateFragment(viewModel.gameButton.draw)
         |+| SceneUpdateFragment(viewModel.resultsButton.draw)
     }
+  end present
+end SceneSplash
 
 final case class SplashSceneViewModel(
 //  splashButton: Button,
-  rulesButton: Button,
-  gameButton: Button,
-  resultsButton: Button 
+    rulesButton: Button,
+    gameButton: Button,
+    resultsButton: Button
 ):
   def update(mouse: Mouse, pointers: Pointers): Outcome[SplashSceneViewModel] =
-    for {
+    for
 //      bn1 <- splashButton.updateFrom(pointers)
       bn2 <- rulesButton.updateFromPointers(pointers)
       bn3 <- gameButton.updateFromPointers(pointers)
       bn4 <- resultsButton.updateFromPointers(pointers)
-    } yield this.copy( /*splashButton = bn1,*/ rulesButton = bn2, gameButton = bn3, resultsButton = bn4)
-
+    yield this.copy( /*splashButton = bn1,*/ rulesButton = bn2, gameButton = bn3, resultsButton = bn4)
+end SplashSceneViewModel
 
 object SplashSceneViewModel:
 
-  val initial: SplashSceneViewModel = 
+  val initial: SplashSceneViewModel =
     SplashSceneViewModel(
-/*      
+      /*
       Button (
         buttonAssets = GameAssets.buttonSplashAssets,
         bounds = Rectangle(20, 20, 240, 80),
         depth = Depth(6)
         ).withUpActions(ButtonSplashEvent),
-*/
+       */
 
-      Button (
+      Button(
         buttonAssets = GameAssets.buttonRulesAssets,
         bounds = Rectangle(50, 50, 240, 80),
         depth = Depth(6)
       ).withUpActions(ButtonRulesEvent),
-
-      Button (
+      Button(
         buttonAssets = GameAssets.buttonGameAssets,
         bounds = Rectangle(50, 150, 240, 80),
         depth = Depth(6)
       ).withUpActions(ButtonGameEvent),
-
-      Button (
+      Button(
         buttonAssets = GameAssets.buttonResultsAssets,
         bounds = Rectangle(50, 250, 240, 80),
         depth = Depth(6)
       ).withUpActions(ButtonResultsEvent)
-    )      
-
+    )
+end SplashSceneViewModel
