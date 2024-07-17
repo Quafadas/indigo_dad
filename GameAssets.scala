2655e236da812ad11ed05d90251a73ab376911cf
MagentaJohn <mrjohnparten@gmail.com>
2024-07-17 18:11:54 +0100

    formatting

--- GameAssets.scala
+++ GameAssets.scala
@@ -5,7 +5,7 @@ import indigoextras.ui.*
 import indigo.shared.materials
 import indigo.shared.materials.Material.ImageEffects
 
-object GameAssets: 
+object GameAssets:
 
   val hxAssetName = "hex2"
   val hxAssetPath = "assets/Hex2.png"
@@ -20,9 +20,8 @@ object GameAssets:
   val cnAssetName = "corners"
   val cnAssetPath = "assets/Corners.png"
 
-
   println("@@@ Object GameAssets START")
-    
+
   def get(): Set[AssetType] =
     Set(
       AssetType.Image(AssetName(hxAssetName), AssetPath(hxAssetPath)),
@@ -30,7 +29,7 @@ object GameAssets:
       AssetType.Image(AssetName(cyAssetName), AssetPath(cyAssetPath)),
       AssetType.Image(AssetName(blAssetName), AssetPath(blAssetPath)),
       AssetType.Image(AssetName(spAssetName), AssetPath(spAssetPath)),
-      AssetType.Image(AssetName(cnAssetName), AssetPath(cnAssetPath))      
+      AssetType.Image(AssetName(cnAssetName), AssetPath(cnAssetPath))
     )
 
   def gHex: Graphic[Material.ImageEffects] =
@@ -66,7 +65,7 @@ object GameAssets:
       down = Graphic(0, 0, 240, 80, 6, Material.Bitmap(AssetName(bnAssetName))).withCrop(720, 160, 240, 80)
     )
 
-  def buttonRulesAssets: ButtonAssets = 
+  def buttonRulesAssets: ButtonAssets =
     ButtonAssets(
       up = Graphic(0, 0, 240, 80, 6, Material.Bitmap(AssetName(bnAssetName))).withCrop(0, 240, 240, 80),
       over = Graphic(0, 0, 240, 80, 6, Material.Bitmap(AssetName(bnAssetName))).withCrop(0, 320, 240, 80),
@@ -80,32 +79,40 @@ object GameAssets:
       down = Graphic(0, 0, 240, 80, 6, Material.Bitmap(AssetName(bnAssetName))).withCrop(960, 160, 90, 80)
     )
 
-
   def splashBg: Graphic[Material.ImageEffects] =
     Graphic(Rectangle(0, 0, 1920, 1080), 2, Material.ImageEffects(AssetName(spAssetName)))
 
-  def cornerTopLeft : Graphic[Material.ImageEffects] =
+  def cornerTopLeft: Graphic[Material.ImageEffects] =
     Graphic(Rectangle(0, 0, 20, 20), 2, Material.ImageEffects(AssetName(cnAssetName)))
 
-  def cornerTopRight : Graphic[Material.ImageEffects] =
+  def cornerTopRight: Graphic[Material.ImageEffects] =
     Graphic(Rectangle(30, 0, 20, 20), 2, Material.ImageEffects(AssetName(cnAssetName)))
 
-  def cornerBottomLeft : Graphic[Material.ImageEffects] =
+  def cornerBottomLeft: Graphic[Material.ImageEffects] =
     Graphic(Rectangle(0, 30, 20, 20), 2, Material.ImageEffects(AssetName(cnAssetName)))
 
-  def cornerBottomRight : Graphic[Material.ImageEffects] =
+  def cornerBottomRight: Graphic[Material.ImageEffects] =
     Graphic(Rectangle(30, 30, 20, 20), 2, Material.ImageEffects(AssetName(cnAssetName)))
 
-  def cornerLayers(width: Int, height: Int, sf: Double, cornerColor: RGBA) : Batch[Graphic[ ImageEffects]] =
-      val layerC1 = (GameAssets.cornerTopLeft).moveTo(0,0).modifyMaterial(_.withTint(cornerColor)).scaleBy(sf,sf)
-      val layerC2 = (GameAssets.cornerTopRight).moveTo(width-20,0).modifyMaterial(_.withTint(cornerColor)).scaleBy(sf,sf)
-      val layerC3 = (GameAssets.cornerBottomLeft).moveTo(0,height-20).modifyMaterial(_.withTint(cornerColor)).scaleBy(sf,sf)
-      val layerC4 = (GameAssets.cornerBottomRight).moveTo(width-20,height-20).modifyMaterial(_.withTint(cornerColor)).scaleBy(sf,sf)
-      Batch(layerC1,layerC2,layerC3,layerC4)    
+  def cornerLayers(width: Int, height: Int, sf: Double, cornerColor: RGBA): Batch[Graphic[ImageEffects]] =
+    val layerC1 = (GameAssets.cornerTopLeft)
+      .moveTo(0, 0)
+      .modifyMaterial(_.withTint(cornerColor))
+      .scaleBy(sf, sf)
+    val layerC2 = (GameAssets.cornerTopRight)
+      .moveTo(width - 20, 0)
+      .modifyMaterial(_.withTint(cornerColor))
+      .scaleBy(sf, sf)
+    val layerC3 = (GameAssets.cornerBottomLeft)
+      .moveTo(0, height - 20)
+      .modifyMaterial(_.withTint(cornerColor))
+      .scaleBy(sf, sf)
+    val layerC4 = (GameAssets.cornerBottomRight)
+      .moveTo(width - 20, height - 20)
+      .modifyMaterial(_.withTint(cornerColor))
+      .scaleBy(sf, sf)
+    Batch(layerC1, layerC2, layerC3, layerC4)
+  end cornerLayers
 
   println("@@@ Object GameAssets FINISH")
-
-
-
-
-
+end GameAssets
